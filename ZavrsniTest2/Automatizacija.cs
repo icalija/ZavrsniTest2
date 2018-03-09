d24aca4d8553b5bf8b0e222c0da16c350e90de5a
Igor Čalija <icalija@live.com>
2018-03-09 21:35:33 +0100

    promene

--- ZavrsniTest2/Automatizacija.cs
+++ ZavrsniTest2/Automatizacija.cs
@@ -49,11 +49,89 @@ namespace Test
         [Test]
         public void Test()
         {
+            GoToURL("https://www.qtptutorial.net/automation-practice/");
+            Sleep(5);
+            IWebElement clcBtn = driver.FindElement(By.Id("idExample"));
+            clcBtn.Click();
 
+
+            bool isSuccess = driver.FindElement(By.ClassName("main_title")).Displayed;
+            string isDisplayed = Convert.ToString(isSuccess);
+            System.IO.File.AppendAllText("C:\\Users\\GIMB\\Desktop\\selenium.txt", "Button succes se prikazao - " + isDisplayed + Environment.NewLine);
+            Sleep(5);
         }
 
+        [Test]
+        public void Test2()
+        {
+            GoToURL("https://www.qtptutorial.net/automation-practice/");
+            Sleep(5);
+            IWebElement clcBtn2 = driver.FindElement(By.Name("NameExample"));
+            clcBtn2.Click();
 
-        [TearDown]
+
+            bool isSuccess2 = driver.FindElement(By.ClassName("xh-highlight")).Displayed;
+            string isDisplayed = Convert.ToString(isSuccess2);
+            System.IO.File.AppendAllText("C:\\Users\\GIMB\\Desktop\\selenium.txt", "Button succes se prikazao - " + isDisplayed + Environment.NewLine);
+            Sleep(5);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            GoToURL("https://www.qtptutorial.net/automation-practice/");
+            Sleep(5);
+            IWebElement clcBtn3 = driver.FindElement(By.ClassName("et_pb_newsletter_button"));
+
+            bool isSuccess3 = clcBtn3.Displayed;
+            string isDisplayed = Convert.ToString(isSuccess3);
+            System.IO.File.AppendAllText("C:\\Users\\GIMB\\Desktop\\selenium.txt", "Email me button se prikazao - " + isDisplayed + Environment.NewLine);
+            Sleep(5);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            GoToURL("https://www.qtptutorial.net/automation-practice/");
+            Sleep(5);
+            IWebElement clcBtn4 = driver.FindElement(By.XPath("//input[@value='Name']"));
+            clcBtn4.SendKeys("Igor");
+
+            IWebElement clcBtn5 = driver.FindElement(By.XPath("//input[@value='Email Address']"));
+            clcBtn4.SendKeys("icalija@live.com");
+            Sleep(5);
+        }
+
+        [Test]
+
+             public void Test5()
+        {
+            GoToURL("https://www.qtptutorial.net/automation-practice/");
+            Sleep(5);
+            IWebElement table = driver.FindElement(By.Id("htmlTableId"));
+            IWebElement uniqueId = table.FindElement(By.XPath("//td[contains(.,'$120, 000 + ')]"));
+
+            Sleep(5);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            GoToURL("https://www.qtptutorial.net/automation-practice/");
+            Sleep(5);
+            IWebElement table = driver.FindElement(By.Id("htmlTableId"));
+            IList<IWebElement> rows = table.FindElements(By.XPath("//tr[contains(.,'Automation Testing Architect')]"));
+
+            foreach (var red in rows)
+            {
+                System.IO.File.AppendAllText("C:\\Users\\GIMB\\Desktop\\selenium.txt", red.Text.ToString() + Environment.NewLine);
+            }
+        }
+     
+
+
+
+    [TearDown]
         public void CloseBrowser()
         {
             driver.Close();
